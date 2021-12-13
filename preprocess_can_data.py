d861b259f67d5ee9d78161017e8a46dbf8bcd7aa
Sinan Demirci <demircis@student.ethz.ch>
2021-12-13 23:01:57 +0100

    -first commit, collect CAN data for all subjects -save data in parquet file -add gitignore -add config file

---
+++ preprocess_can_data.py
@@ -0,0 +1,108 @@
+import pandas as pd
+from yaml import load, Loader
+import glob
+import re
+import datetime
+
+from bunch import Bunch
+
+stream = open("config.yaml", 'r')
+config = Bunch(load(stream, Loader=Loader))
+
+CAN_COLUMNS = ['interval', 'steer', 'latpos', 'gas', 'brake', 'clutch', 'Thw', 'velocity', 'acc', 'latvel', 'dtoint', 'indicator',
+               'heading', 'SpeedDif', 'LaneDirection', 'SteerError', 'SteerSpeed', 'Ttc', 'TtcOpp', 'LeftDis',
+               'RightDis', 'AheadDis', 'traflight', 'handbrake', 'engine']
+
+if config.full_study:
+    DATA_FOLDER = "/mnt/adar/drive/study"
+    CAN_COLUMNS = CAN_COLUMNS[:22] + ["ypos", "xpos", "YawRate"] + CAN_COLUMNS[22:]
+else:
+    DATA_FOLDER = "/mnt/adar/drive/pilot"
+
+def filter_can_data_engine_on(can_data, timestamps):
+    whole_trip_timestamp = timestamps[timestamps['reason'] == 'trip_start_end_times'].iloc[0]
+    can_data_filtered = can_data[(can_data['timestamp'] > whole_trip_timestamp['start_time']) &
+        (can_data['timestamp'] < whole_trip_timestamp['end_time'])]
+
+    assert len(can_data_filtered[(can_data_filtered['timestamp'] < whole_trip_timestamp['start_time'])
+        & (can_data_filtered['timestamp'] > whole_trip_timestamp['end_time'])].index) == 0
+
+    engine_in_trip_off_timestamps = timestamps[timestamps['reason'] == 'engine_in_trip_off']
+
+    for start_time, end_time in engine_in_trip_off_timestamps[['start_time', 'end_time']].to_numpy():
+        can_data_filtered = can_data_filtered[(can_data_filtered['timestamp'] < start_time) |
+            (can_data_filtered['timestamp'] > end_time)]
+    
+        assert len(can_data_filtered[(can_data_filtered['timestamp']> start_time)
+            & (can_data_filtered['timestamp'] < end_time)].index) == 0
+
+    return can_data_filtered
+
+if __name__ == '__main__':
+    data = []
+
+    if config.full_study:
+        subject_folders = sorted(glob.glob(DATA_FOLDER + '/*_sober') + glob.glob(DATA_FOLDER + '/*_above') + glob.glob(
+            DATA_FOLDER + '/*_below'))
+        subject_folders = [f for f in subject_folders if 'audio' not in f]
+    else:
+        subject_folders = sorted(glob.glob(DATA_FOLDER + '/*_drunk') + glob.glob(DATA_FOLDER + '/*_sober'))
+
+    subject_re = re.compile('d-([0-9]+)')
+    scenario_re = re.compile(r's\d{2}[abcde]-(\w*)Exp.dat$')
+    for subject in subject_folders:
+        subject_id_match = subject_re.search(subject)
+        if not subject_id_match:
+            continue
+        subject_id = subject_id_match.group(1)
+
+        if "drunk" in subject:
+            state = "drunk"
+        elif "above" in subject:
+            state = "above"
+        elif "below" in subject:
+            state = "below"
+        else:
+            state = "sober"
+
+        timestamp_file = glob.glob(subject + '/timestamps.csv')
+        timestamps = pd.read_csv(timestamp_file[0], sep=',', index_col=0, skiprows=0,
+                            parse_dates=['start_time', 'end_time'])
+        
+        for can_file in sorted(glob.glob(subject + '/simulator/*.dat')):
+            scenario_re_match = scenario_re.search(can_file)
+
+            if not scenario_re_match:
+                continue
+            scenario = scenario_re_match.group(1).lower()
+
+            print('subject id: {}, state: {}, scenario: {}'.format(subject_id, state, scenario))
+        
+            with open(can_file) as f:
+                line = f.readline().replace('\n', '')
+                line = line.replace('.   ', '.000').replace('.  ', '.00').replace('. ', '.0')
+                simulator_timestamp = datetime.datetime.strptime(line, '%Y-%m-%d %H:%M:%S.%f')
+
+            can_data = pd.read_csv(can_file, header=None, index_col=None, names=CAN_COLUMNS, delim_whitespace=True, 
+                skiprows=2, na_values='9999.00')
+            can_data['interval'] = [pd.to_datetime(simulator_timestamp + datetime.timedelta(milliseconds=interval * 1000))
+                .tz_localize('Europe/Zurich') for interval in can_data['interval']]
+            can_data.rename(columns={'interval': 'timestamp'}, inplace=True)
+
+            timestamps_scenario = timestamps[timestamps['scenario'] == scenario]
+
+            can_data_filtered = filter_can_data_engine_on(can_data, timestamps_scenario)
+
+            can_data_filtered.insert(0, 'subject_id', subject_id)
+            can_data_filtered.insert(1, 'subject_state', state)
+            can_data_filtered.insert(2, 'subject_scenario', scenario)
+            can_data_filtered.reset_index(drop=True, inplace=True)
+
+            can_data_filtered.loc[:, "indicator_right"] = (can_data_filtered["indicator"] == 1).astype(int)
+            can_data_filtered.loc[:, "indicator_left"] = (can_data_filtered["indicator"] == 2).astype(int)
+            can_data_filtered = can_data_filtered.drop(["indicator"], axis=1)
+
+            data.append(can_data_filtered)
+    
+    data = pd.concat(data)
+    data.to_parquet("out/can_data.parquet")
