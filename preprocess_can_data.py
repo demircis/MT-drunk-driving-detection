17caab6964f4d9fccb2fb887325e9569589a6ffc
Sinan Demirci <demircis@student.ethz.ch>
2022-01-12 18:19:14 +0100

    -extract path and segment ids from video -refactoring

--- preprocess_can_data.py
+++ preprocess_can_data.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
-from scipy.spatial import KDTree
 import glob
 import re
 import datetime
-import math
+import pytz
+import cv2 as cv
+
 
 def filter_can_data_engine_on(can_data, timestamps):
     whole_trip_timestamp = timestamps[timestamps['reason'] == 'trip_start_end_times'].iloc[0]
@@ -34,23 +35,112 @@ def split_signal_in_right_left(df, signal):
     return df
 
 
-def find_closest_segment(lanes_df, target_xpos, target_ypos):
-    distances = [[math.sqrt((target_xpos - x)**2 + (target_ypos - y)**2) for x, y in zip(np.linspace(start_x, end_x, 10), np.linspace(start_y, end_y, 10))]
-        for start_x, start_y, end_x, end_y in zip(lanes_df['StartPos_x_segment'], lanes_df['StartPos_y_segment'], lanes_df['StartPos_x_segment'], lanes_df['EndPos_y_segment'])]
-    min_per_segment = np.min(distances, axis=1)
-    min_idx = np.argmin(min_per_segment)
+def map_to_digit(segments):
+    zero = np.array([1, 1, 1, 0, 1, 1, 1])
+    one = np.array([0, 0, 1, 0, 0, 1, 0])
+    two = np.array([1, 0, 1, 1, 1, 0, 1])
+    three = np.array([1, 0, 1, 1, 0, 1, 1])
+    four = np.array([0, 1, 1, 1, 0, 1, 0])
+    five = np.array([1, 1, 0, 1, 0, 1, 1])
+    six = np.array([1, 1, 0, 1, 1, 1, 0])
+    seven = np.array([1, 0, 1, 0, 0, 1, 0])
+    eight = np.array([1, 1, 1, 1, 1, 1, 1])
+    nine = np.array([1, 1, 1, 1, 0, 1, 0])
 
-    # points = lanes_df[['StartPos_x_segment', 'StartPos_y_segment']].to_numpy()
-    # points = np.concatenate((points, lanes_df[['EndPos_x_segment', 'EndPos_y_segment']].to_numpy()))
-    # _, min_idx = KDTree(points).query([target_xpos, target_ypos])
-    # nr_segments = len(lanes_df.index.to_numpy())
-    # if min_idx >= nr_segments:
-    #     min_idx = min_idx - nr_segments
-    return lanes_df.iloc[min_idx]['segment_id']
+    if np.array_equal(segments, zero):
+        return '0'
+    elif np.array_equal(segments, one):
+        return '1'
+    elif np.array_equal(segments, two):
+        return '2'
+    elif np.array_equal(segments, three):
+        return '3'
+    elif np.array_equal(segments, four):
+        return '4'
+    elif np.array_equal(segments, five):
+        return '5'
+    elif np.array_equal(segments, six):
+        return '6'
+    elif np.array_equal(segments, seven):
+        return '7'
+    elif np.array_equal(segments, eight):
+        return '8'
+    elif np.array_equal(segments, nine):
+        return '9'
 
 
-def calculate_lane_pos(lanes_df, target_xpos, target_ypos, latpos):
-    segment_id = find_closest_segment(lanes_df.drop_duplicates(subset='segment_id'), target_xpos, target_ypos)
+def get_segments_states(digits):
+    segments_states = []
+    for i in range(len(digits)):
+        digit = digits[i]
+        contours, _ = cv.findContours(digit, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        if len(contours) == 0:
+            continue
+        full_contours = np.concatenate(contours)
+        x, y, w, h = cv.boundingRect(full_contours)
+        digit_rect = digit[y:y + h, x:x + w]
+        (digit_h, digit_w) = digit_rect.shape
+        (segment_w, segment_h) = (int(digit_w * 0.25), int(digit_h * 0.15))
+        segment_h_center = int(digit_h * 0.10)
+        segments = [
+            ((0, 0), (w, segment_h)),	# top
+            ((0, 0), (segment_w, h // 2)),	# top-left
+            ((w - segment_w, 0), (w, h // 2)),	# top-right
+            ((0, (h // 2) - segment_h_center) , (w, (h // 2) + segment_h_center)), # center
+            ((0, h // 2), (segment_w, h)),	# bottom-left
+            ((w - segment_w, h // 2), (w, h)),	# bottom-right
+            ((0, h - segment_h), (w, h))	# bottom
+        ]
+        segment_state = np.array([0] * len(segments))
+        for j, ((xA, yA), (xB, yB)) in enumerate(segments):
+            segment = digit_rect[yA:yB, xA:xB]
+            total = cv.countNonZero(segment)
+            area = (xB - xA) * (yB - yA)
+            if total / float(area) > 0.5:
+                segment_state[j]= 1
+        segments_states.append(segment_state)
+    
+    return segments_states
+
+
+def get_path_and_segment_ids(video, data_timestamps, video_timestamp):
+    can_data_timestamps_ms = (data_timestamps - video_timestamp) / datetime.timedelta(milliseconds=1)
+    cap = cv.VideoCapture(video)
+    path_ids = []
+    segment_ids = []
+    for timestamp_ms in can_data_timestamps_ms:
+        is_set = cap.set(cv.CAP_PROP_POS_MSEC, timestamp_ms)
+        if is_set:
+            success, frame = cap.read()
+            if success:
+                path_img = frame[1723:1743, 1022:1098, :]
+                imgray = cv.cvtColor(path_img, cv.COLOR_BGR2GRAY)
+                _, thresh = cv.threshold(imgray, 127, 255, cv.THRESH_BINARY)
+                digits = np.array_split(thresh, 5, axis=1)
+                path_id = ''
+                for segment_state in get_segments_states(digits):
+                    path_id += map_to_digit(segment_state)
+                path_id = int(path_id)
+                path_ids.append(path_id)
+
+                segment_img = frame[1752:1772, 1022:1098, :]
+                imgray = cv.cvtColor(segment_img, cv.COLOR_BGR2GRAY)
+                _, thresh = cv.threshold(imgray, 127, 255, cv.THRESH_BINARY)
+                digits = np.array_split(thresh, 5, axis=1)
+                segment_id = ''
+                for segment_state in get_segments_states(digits):
+                    segment_id += map_to_digit(segment_state)
+                segment_id = int(segment_id)
+                segment_ids.append(segment_id)
+            else:
+                print('could not get frame')
+        else:
+            print('could not set video timestamp')
+    
+    return np.array(path_ids), np.array(segment_ids)
+
+
+def calculate_lane_pos(lanes_df, segment_id, latpos):
     lanes_in_segment = lanes_df.loc[lanes_df['segment_id'] == segment_id]
     lanes_center = []
     prev_width = 0
@@ -64,7 +154,7 @@ def calculate_lane_pos(lanes_df, target_xpos, target_ypos, latpos):
         lane_position = latpos
     else:
         lane_position = latpos - (lanes_center[lane_number] - lanes_center[0])
-    return segment_id, lane_number, lane_position
+    return lane_number, lane_position
 
 
 def do_derivation_of_signals(df, signals, suffix, frequency_hz=None, replace_suffix=None):
@@ -119,6 +209,7 @@ def do_preprocessing(full_study, overwrite, data_freq=30):
     
     subject_re = re.compile('d-([0-9]+)')
     scenario_re = re.compile(r's\d{2}[abcde]-(\w*)Exp.dat$')
+    timestamp_re = re.compile(r'(\d{4})-(\d{2})-(\d{2})--(\d{2})-(\d{2})-(\d{2}).flv')
 
     lanes_df = pd.read_csv('out/scenario_information.csv')
 
@@ -140,6 +231,19 @@ def do_preprocessing(full_study, overwrite, data_freq=30):
         timestamp_file = glob.glob(subject + '/timestamps.csv')
         timestamps = pd.read_csv(timestamp_file[0], sep=',', index_col=0, skiprows=0,
                             parse_dates=['start_time', 'end_time'])
+
+        video = glob.glob(subject + '/obs-videos/*.flv')[0]
+        match = timestamp_re.search(video.split('/')[-1])
+        if match:
+            year = int(match.group(1))
+            month = int(match.group(2))
+            day = int(match.group(3))
+            hour = int(match.group(4))
+            minute = int(match.group(5))
+            second = int(match.group(6))
+            video_timestamp = datetime.datetime(year, month, day, hour, minute, second)
+            tz = pytz.timezone('Europe/Zurich')
+            video_timestamp = tz.localize(video_timestamp)
         
         for can_file in sorted(glob.glob(subject + '/simulator/*.dat')):
             scenario_re_match = scenario_re.search(can_file)
@@ -174,13 +278,17 @@ def do_preprocessing(full_study, overwrite, data_freq=30):
             can_data_filtered.loc[:, "indicator_left"] = (can_data_filtered["indicator"] == 2).astype(int)
             can_data_filtered.drop(["indicator"], axis=1, inplace=True)
 
+            path_ids, segment_ids = get_path_and_segment_ids(video, can_data_filtered['timestamp'], video_timestamp)
+            can_data_filtered.loc[:, 'path_id'] = path_ids
+            can_data_filtered.loc[:, 'segment_id'] = segment_ids
+
             lanes_on_route = lanes_df.loc[(lanes_df['scenario'] == scenario) & (lanes_df['lane_belongs_to_route'] == True)]
-            lane_info = [calculate_lane_pos(lanes_on_route, xpos, ypos, latpos) for xpos, ypos, latpos in 
-                zip(can_data_filtered['xpos'], can_data_filtered['ypos'], can_data_filtered['latpos'])]
+            lane_info = [calculate_lane_pos(lanes_on_route, segment_id, latpos)
+                    for segment_id, latpos in zip(can_data_filtered['segment_id'], can_data_filtered['latpos'])]
             lane_info = np.array(lane_info)
-            can_data_filtered.loc[:, 'segment_id'] = lane_info[:, 0]
-            can_data_filtered.loc[:, 'lane_number'] = lane_info[:, 1]
-            can_data_filtered.loc[:, 'lane_position'] = lane_info[:, 2]
+            
+            can_data_filtered.loc[:, 'lane_number'] = lane_info[:, 0]
+            can_data_filtered.loc[:, 'lane_position'] = lane_info[:, 1]
 
             can_data_filtered.loc[:, 'Dhw'] = can_data_filtered['Thw'] * can_data_filtered['velocity']
 
