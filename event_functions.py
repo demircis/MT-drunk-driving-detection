-replace None by NaN for ids -adjust features calculation to take ms -use flirt impl. for event stats calculation -fix index for event rows -add feature analysis notebook (kdeplots) -refactoring

event_functions.py
@@ -1,58 +1,42 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import kurtosis
-from scipy.signal import find_peaks, peak_widths
+
+from get_features import get_features
 
 def calculate_event_stats(events, signal):
-    def start(x):
-        return x.head(1)
-    def end(x):
-        return x.tail(1)
     def duration(x):
-        return (x.max()-x.min()).total_seconds()
-    def quartile(x):
-        return np.quantile(x, q=0.25) if not x.empty else np.nan
-    def autocorr(x):
-        return x.autocorr() if not x.empty else np.nan
-    def mean_peak_heights(x):
-        _, props = find_peaks(x, height=0, width=1, plateau_size=0)
-        return np.mean(props['peak_heights']) if not len(props['peak_heights']) == 0 else np.nan
-    def mean_peak_widths(x):
-        ind, _ = find_peaks(x, height=0, width=1, plateau_size=0)
-        widths, _, _, _ = peak_widths(x, ind)
-        return np.mean(widths) if not len(widths) == 0 else np.nan
-    
-    return events.agg(
-            {
-                'timestamp': [start, duration],
-                signal: ['mean', 'min', 'max', 'std', 'skew', kurtosis, quartile, autocorr, mean_peak_heights, mean_peak_widths],
-                'velocity': [start, 'mean', 'std', end],
-                'acc': [start, 'mean', 'std', end],
-                'steer': [start, 'mean', 'std', end],
-                'SteerSpeed': [start, 'mean', 'std', end],
-                'latvel': [start, 'mean', 'std', end]
-            }
-        )
+        first = x.index.to_numpy()[0]
+        last = x.index.to_numpy()[-1]
+        return int((x['timestamp'].at[last] - x['timestamp'].at[first]).total_seconds() * 1000)
+
+      
+    return events.apply(lambda x: get_features(x, duration(x), num_cores=1, step_size=str(duration(x)) + 'ms' if duration(x) != 0 else '1S'))
 
 
 def brake_to_gas(x):
     gas = x[x['gas'] > 0]['gas']
+    duration = 0
     if not gas.empty:
         ind = gas.index.to_numpy()[0]
         first = x['timestamp'].index.to_numpy()[0]
-        return pd.Series({'timestamp': x['timestamp'].at[first], 'brake_to_gas': (x['timestamp'].at[ind] - x['timestamp'].at[first]).total_seconds()})
+        duration = int((x['timestamp'].at[ind] - x['timestamp'].at[first]).total_seconds() * 1000)
+    if duration != 0:
+        return get_features(x, duration, num_cores=1, step_size=str(duration) + 'ms')
     else:
-        return pd.Series({'timestamp': np.nan, 'brake_to_gas': np.nan})
+        return get_features(x, duration, num_cores=1)
 
 
 def gas_to_brake(x):
     brake = x[x['brake'] > 0]['brake']
+    duration = 0
     if not brake.empty:
         ind = brake.index.to_numpy()[0]
         first = x['timestamp'].index.to_numpy()[0]
-        return pd.Series({'timestamp': x['timestamp'].at[first], 'gas_to_brake': (x['timestamp'].at[ind] - x['timestamp'].at[first]).total_seconds()})
+        duration = int((x['timestamp'].at[ind] - x['timestamp'].at[first]).total_seconds() * 1000)
+    if duration != 0:
+        return get_features(x, duration, num_cores=1, step_size=str(duration) + 'ms')
     else:
-        return pd.Series({'timestamp': np.nan, 'gas_to_brake': np.nan})
+        return get_features(x, duration, num_cores=1)
 
 
 def distance_covered(x):
@@ -67,89 +51,15 @@ def distance_covered(x):
 def get_overtaking_events(data, groupby):
     def overtaking_event(group):
         # 5 seconds before and after lane switch
+        duration = 0
         if not (group.head(1).empty or group.tail(1).empty):
-            start = max(group.head(1).index.to_numpy()[0] - 150, 0)
-            end = min(group.tail(1).index.to_numpy()[0] + 150, data.index.max())
-            timestamp = data.iloc[start]['timestamp']
-            duration = (data.iloc[end]['timestamp'] - data.iloc[start]['timestamp']).total_seconds()
-            distance = data.iloc[start:end]['velocity'].mean() * duration
-            mean_lane_pos = data.iloc[start:end]['lane_position'].mean()
-            std_lane_pos = data.iloc[start:end]['lane_position'].std()
-            min_velocity = data.iloc[start:end]['velocity'].min()
-            max_velocity = data.iloc[start:end]['velocity'].max()
-            mean_velocity = data.iloc[start:end]['velocity'].mean()
-            std_velocity = data.iloc[start:end]['velocity'].std()
-            min_acc = data.iloc[start:end]['acc'].min()
-            max_acc = data.iloc[start:end]['acc'].max()
-            mean_acc = data.iloc[start:end]['acc'].mean()
-            std_acc = data.iloc[start:end]['acc'].std()
-            min_steer = data.iloc[start:end]['steer'].min()
-            max_steer = data.iloc[start:end]['steer'].max()
-            mean_steer = data.iloc[start:end]['steer'].mean()
-            std_steer = data.iloc[start:end]['steer'].std()
-            min_SteerSpeed = data.iloc[start:end]['SteerSpeed'].min()
-            max_SteerSpeed = data.iloc[start:end]['SteerSpeed'].max()
-            mean_SteerSpeed = data.iloc[start:end]['SteerSpeed'].mean()
-            std_SteerSpeed = data.iloc[start:end]['SteerSpeed'].std()
-            min_latvel = data.iloc[start:end]['latvel'].min()
-            max_latvel = data.iloc[start:end]['latvel'].max()
-            mean_latvel = data.iloc[start:end]['latvel'].mean()
-            std_latvel = data.iloc[start:end]['latvel'].std()
-            return pd.Series({
-                'timestamp': timestamp,
-                'duration': duration,
-                'distance': distance,
-                'mean_lane_position': mean_lane_pos,
-                'std_lane_position': std_lane_pos,
-                'min_velocity': min_velocity,
-                'max_velocity': max_velocity,
-                'mean_velocity': mean_velocity,
-                'std_velocity': std_velocity,
-                'min_acc': min_acc,
-                'max_acc': max_acc,
-                'mean_acc': mean_acc,
-                'std_acc': std_acc,
-                'min_steer': min_steer,
-                'max_steer': max_steer,
-                'mean_steer': mean_steer,
-                'std_steer': std_steer,
-                'min_SteerSpeed': min_SteerSpeed,
-                'max_SteerSpeed': max_SteerSpeed,
-                'mean_SteerSpeed': mean_SteerSpeed,
-                'std_SteerSpeed': std_SteerSpeed,
-                'min_latvel': min_latvel,
-                'max_latvel': max_latvel,
-                'mean_latvel': mean_latvel,
-                'std_latvel': std_latvel
-                })
+            start = max(group.index.to_numpy()[0] - 150, 0)
+            end = min(group.index.to_numpy()[-1] + 150, data.shape[0]-1)
+            duration = int((data.iloc[end]['timestamp'] - data.iloc[start]['timestamp']).total_seconds() * 1000)
+        if duration != 0:
+            return get_features(data.iloc[start:end], duration, num_cores=1, step_size=str(duration) + 'ms')
         else:
-            return pd.Series({
-                'timestamp': np.nan,
-                'duration': np.nan,
-                'distance': distance,
-                'mean_lane_position': mean_lane_pos,
-                'std_lane_position': std_lane_pos,
-                'min_velocity': np.nan,
-                'max_velocity': np.nan,
-                'mean_velocity': np.nan,
-                'std_velocity': np.nan,
-                'min_acc': np.nan,
-                'max_acc': np.nan,
-                'mean_acc': np.nan,
-                'std_acc': np.nan,
-                'min_steer': np.nan,
-                'max_steer': np.nan,
-                'mean_steer': np.nan,
-                'std_steer': np.nan,
-                'min_SteerSpeed': np.nan,
-                'max_SteerSpeed': np.nan,
-                'mean_SteerSpeed': np.nan,
-                'std_SteerSpeed': np.nan,
-                'min_latvel': np.nan,
-                'max_latvel': np.nan,
-                'mean_latvel': np.nan,
-                'std_latvel': np.nan
-                })
+            return get_features(data.iloc[start:end], duration, num_cores=1)
     return groupby.apply(overtaking_event)
 
 
@@ -179,59 +89,20 @@ def get_turning_events(data, subject_id, subject_state, subject_scenario):
     for maneuver_indices in maneuvers:
         start = maneuver_indices[0]
         end = maneuver_indices[-1]
-        timestamp = data.iloc[start]['timestamp']
-        duration = (data.iloc[end]['timestamp'] - data.iloc[start]['timestamp']).total_seconds()
-        distance = data.iloc[start:end]['velocity'].mean() * duration
-        min_velocity = data.iloc[start:end]['velocity'].min()
-        max_velocity = data.iloc[start:end]['velocity'].max()
-        mean_velocity = data.iloc[start:end]['velocity'].mean()
-        std_velocity = data.iloc[start:end]['velocity'].std()
-        min_acc = data.iloc[start:end]['acc'].min()
-        max_acc = data.iloc[start:end]['acc'].max()
-        mean_acc = data.iloc[start:end]['acc'].mean()
-        std_acc = data.iloc[start:end]['acc'].std()
-        min_steer = data.iloc[start:end]['steer'].min()
-        max_steer = data.iloc[start:end]['steer'].max()
-        mean_steer = data.iloc[start:end]['steer'].mean()
-        std_steer = data.iloc[start:end]['steer'].std()
-        min_SteerSpeed = data.iloc[start:end]['SteerSpeed'].min()
-        max_SteerSpeed = data.iloc[start:end]['SteerSpeed'].max()
-        mean_SteerSpeed = data.iloc[start:end]['SteerSpeed'].mean()
-        std_SteerSpeed = data.iloc[start:end]['SteerSpeed'].std()
-        min_latvel = data.iloc[start:end]['latvel'].min()
-        max_latvel = data.iloc[start:end]['latvel'].max()
-        mean_latvel = data.iloc[start:end]['latvel'].mean()
-        std_latvel = data.iloc[start:end]['latvel'].std()
-        turning_event_data.append({
-            'timestamp': timestamp,
-            'duration': duration,
-            'distance': distance,
-            'min_velocity': min_velocity,
-            'max_velocity': max_velocity,
-            'mean_velocity': mean_velocity,
-            'std_velocity': std_velocity,
-            'min_acc': min_acc,
-            'max_acc': max_acc,
-            'mean_acc': mean_acc,
-            'std_acc': std_acc,
-            'min_steer': min_steer,
-            'max_steer': max_steer,
-            'mean_steer': mean_steer,
-            'std_steer': std_steer,
-            'min_SteerSpeed': min_SteerSpeed,
-            'max_SteerSpeed': max_SteerSpeed,
-            'mean_SteerSpeed': mean_SteerSpeed,
-            'std_SteerSpeed': std_SteerSpeed,
-            'min_latvel': min_latvel,
-            'max_latvel': max_latvel,
-            'mean_latvel': mean_latvel,
-            'std_latvel': std_latvel
-            })
-    
-    df = pd.DataFrame(turning_event_data)
+        duration = int((data.loc[end, 'timestamp'] - data.loc[start, 'timestamp']).total_seconds() * 1000)
+        if duration != 0:
+            turning_event_data.append(get_features(data.loc[start:end], duration, num_cores=1, step_size=str(duration) + 'ms'))
+        else:
+            turning_event_data.append(get_features(data.loc[start:end], duration, num_cores=1))
+    if len(turning_event_data) == 0:
+        return pd.DataFrame()
+    df = pd.concat(turning_event_data, axis=0)
+    df.dropna(axis=0, how='all', inplace=True)
+    df.reset_index(level=0, inplace=True)
     df.insert(0, 'subject_id', subject_id)
     df.insert(1, 'subject_state', subject_state)
     df.insert(2, 'subject_scenario', subject_scenario)
+    df.set_index(['subject_id', 'subject_state', 'subject_scenario', 'datetime'], drop=True, inplace=True)
     return df
 
 
@@ -246,42 +117,19 @@ def get_road_sign_events(sign_info, data, sign_type, subject_id, subject_state, 
     for i, idx in enumerate(indices_for_sign):
         if distances[i][idx] <= 50:
             start = max(0, idx-150)
-            end = min(idx+150, data.tail(1).index.to_numpy()[0])
-            timestamp = data.iloc[idx]['timestamp']
-            duration = (data.iloc[end]['timestamp'] - data.iloc[start]['timestamp']).total_seconds()
-            distance = data.iloc[start:end]['velocity'].mean() * duration
-            min_velocity = data.iloc[start:end]['velocity'].min()
-            max_velocity = data.iloc[start:end]['velocity'].max()
-            mean_velocity = data.iloc[start:end]['velocity'].mean()
-            std_velocity = data.iloc[start:end]['velocity'].std()
-            min_acc = data.iloc[start:end]['acc'].min()
-            max_acc = data.iloc[start:end]['acc'].max()
-            mean_acc = data.iloc[start:end]['acc'].mean()
-            std_acc = data.iloc[start:end]['acc'].std()
-            min_brake = data.iloc[start:end]['brake'].min()
-            max_brake = data.iloc[start:end]['brake'].max()
-            mean_brake = data.iloc[start:end]['brake'].mean()
-            std_brake = data.iloc[start:end]['brake'].std()
-            road_sign_event_stats.append({
-                'timestamp': timestamp,
-                'sign': sign_type,
-                'duration': duration,
-                'distance': distance,
-                'min_velocity': min_velocity,
-                'max_velocity': max_velocity,
-                'mean_velocity': mean_velocity,
-                'std_velocity': std_velocity,
-                'min_acc': min_acc,
-                'max_acc': max_acc,
-                'mean_acc': mean_acc,
-                'std_acc': std_acc,
-                'min_brake': min_brake,
-                'max_brake': max_brake,
-                'mean_brake': mean_brake,
-                'std_brake': std_brake
-                })
-    df = pd.DataFrame(road_sign_event_stats)
+            end = min(idx+150, data.shape[0]-1)
+            duration = int((data.iloc[end]['timestamp'] - data.iloc[start]['timestamp']).total_seconds() * 1000)
+            if duration != 0:
+                road_sign_event_stats.append(get_features(data.iloc[start:end], duration, num_cores=1, step_size=str(duration) + 'ms'))
+            else:
+                road_sign_event_stats.append(get_features(data.iloc[start:end], duration, num_cores=1))
+    if len(road_sign_event_stats) == 0:
+        return pd.DataFrame()
+    df = pd.concat(road_sign_event_stats, axis=0)
+    df.dropna(axis=0, how='all', inplace=True)
+    df.reset_index(level=0, inplace=True)
     df.insert(0, 'subject_id', subject_id)
     df.insert(1, 'subject_state', subject_state)
     df.insert(2, 'subject_scenario', subject_scenario)
+    df.set_index(['subject_id', 'subject_state', 'subject_scenario', 'datetime'], drop=True, inplace=True)
     return df

get_features.py
@@ -9,9 +9,10 @@ from joblib import Parallel, delayed
 def get_features(data, epoch_width=60, num_cores=0, step_size="1S"):
     if not num_cores >= 1:
         num_cores = multiprocessing.cpu_count()
-    print('using # cores: ', num_cores)
+    #print('using # cores: ', num_cores)
 
     input_data = data.copy()
+    input_data.set_index('timestamp', inplace=True)
 
     start_time = input_data.index.min()
     end_time = input_data.index.max()
@@ -30,12 +31,14 @@ def get_features(data, epoch_width=60, num_cores=0, step_size="1S"):
 def get_sliding_window(data, epoch_width, i):
 
     min_timestamp = i
-    max_timestamp = min_timestamp + timedelta(seconds=epoch_width)
+    max_timestamp = min_timestamp + timedelta(milliseconds=epoch_width * 1000)
     results = {
         'datetime': min_timestamp,
     }
 
-    relevant_data = data.loc[(data.index >= min_timestamp) & (data.index < max_timestamp)]
+    relevant_data = data.loc[(data.index >= min_timestamp) & (data.index < min_timestamp)]
+    if epoch_width != 0:
+        relevant_data = data.loc[(data.index >= min_timestamp) & (data.index < max_timestamp)]
 
     for column in relevant_data.columns:
         column_results = get_stats(relevant_data[column], column)
@@ -44,7 +47,7 @@ def get_sliding_window(data, epoch_width, i):
     return results
 
 
-def get_stats(data, key_suffix=None):
+def get_stats(data, key_prefix=None):
     results = {
         'mean': np.nan,
         'std': np.nan,
@@ -82,8 +85,7 @@ def get_stats(data, key_suffix=None):
         results['rms'] = np.sqrt(results['energy'] / len(data))
         results['lineintegral'] = np.abs(np.diff(data)).sum()
 
-        if key_suffix is not None:
-            results = {k + '_' + key_suffix: v for k, v in results.items()}
-
+    if key_prefix is not None:
+        results = {key_prefix + '_' + k: v for k, v in results.items()}
 
     return results

id_extraction.py
@@ -140,8 +140,8 @@ def crop_video(video, crop_dimensions):
 def get_path_and_segment_ids(video, dimensions, data_timestamps, video_timestamp, data_freq):
     can_data_timestamps_ms = ((data_timestamps - video_timestamp) / datetime.timedelta(milliseconds=1)).to_numpy()
     nr_timestamps = len(can_data_timestamps_ms)
-    path_ids = np.array([None] * nr_timestamps)
-    segment_ids = np.array([None] * nr_timestamps)
+    path_ids = np.array([np.nan] * nr_timestamps)
+    segment_ids = np.array([np.nan] * nr_timestamps)
 
     sampling_indices = np.arange(0, nr_timestamps, data_freq * 5)
     last_index = nr_timestamps-1
@@ -157,7 +157,7 @@ def get_path_and_segment_ids(video, dimensions, data_timestamps, video_timestamp
     path_ids, segment_ids = get_ids_for_indices(path_ids, segment_ids, cropped_video, dimensions, can_data_timestamps_ms, sampling_indices)
 
     prev_none = -1
-    while len(segment_ids[segment_ids == None]) != 0:
+    while len(segment_ids[np.isnan(segment_ids)]) != 0:
         new_indices = []
         for i in range(len(sampling_indices)-1):
             left = sampling_indices[i]
@@ -174,11 +174,11 @@ def get_path_and_segment_ids(video, dimensions, data_timestamps, video_timestamp
         sampling_indices = np.concatenate((sampling_indices, new_indices))
         sampling_indices = np.sort(sampling_indices)
         path_ids, segment_ids = get_ids_for_indices(path_ids, segment_ids, cropped_video, dimensions, can_data_timestamps_ms, new_indices)
-        sampling_indices = sampling_indices[segment_ids[sampling_indices] != None]
+        sampling_indices = sampling_indices[np.invert(np.isnan(segment_ids[sampling_indices]))]
 
-        if len(segment_ids[segment_ids == None]) == prev_none:
+        if len(segment_ids[np.isnan(segment_ids)]) == prev_none:
             break
-        prev_none = len(segment_ids[segment_ids == None])
+        prev_none = len(segment_ids[np.isnan(segment_ids)])
 
     return path_ids, segment_ids
 