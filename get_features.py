-fix manual feature calculation bug (set_index) -simplify can data feature generation -add progress bar to feature generation

get_features.py
@@ -5,6 +5,7 @@ from scipy.signal import find_peaks
 from scipy.stats import skew, kurtosis, iqr
 import multiprocessing
 from joblib import Parallel, delayed
+from tqdm import tqdm
 
 def get_features(data, epoch_width=60, num_cores=0, step_size="1S"):
     if not num_cores >= 1:
@@ -20,7 +21,7 @@ def get_features(data, epoch_width=60, num_cores=0, step_size="1S"):
     inputs = pd.date_range(start_time, end_time, freq=step_size)
 
     results = Parallel(n_jobs=num_cores, backend='multiprocessing')(
-        delayed(get_sliding_window)(input_data, epoch_width=epoch_width, i=k) for k in inputs)
+        delayed(get_sliding_window)(input_data, epoch_width=epoch_width, i=k) for k in tqdm(inputs))
     results = pd.DataFrame(list(filter(None, results)))  # filter out None values
     results.set_index('datetime', inplace=True)
     results.sort_index(inplace=True)
@@ -41,14 +42,15 @@ def get_sliding_window(data, epoch_width, i):
         relevant_data = data.loc[(data.index >= min_timestamp) & (data.index < max_timestamp)]
 
     for column in relevant_data.columns:
-        column_results = get_stats(relevant_data[column], column)
+        column_results = get_stats(relevant_data[column], epoch_width, column)
         results.update(column_results)
 
     return results
 
 
-def get_stats(data, key_prefix=None):
+def get_stats(data, duration, key_prefix=None):
     results = {
+        'duration': np.nan,
         'mean': np.nan,
         'std': np.nan,
         'min': np.nan,
@@ -68,6 +70,7 @@ def get_stats(data, key_prefix=None):
 
     if len(data) > 0:
 
+        results['duration'] = duration
         results['mean'] = np.mean(data)
         results['std'] = np.std(data)
         results['min'] = np.min(data)